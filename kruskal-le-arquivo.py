29c7479094b5a2bbfeff00e08af69439f501a5ac
Arthur Vinicius <thuur.vss@gmail.com>
2021-11-10 13:49:20 -0300

    Tentativa de implementação do algoritmo de Kruskal

--- kruskal-le-arquivo.py
+++ kruskal-le-arquivo.py
@@ -7,18 +7,40 @@ cidadeDistancia = []
 with open('cidadedistancia.csv', 'r') as csvfile:
     for line in csvfile:
         l = line.strip().split(';')
-        dis = {
-            'distanc': float(l[2]),
-            'origem' :  l[0],
-            'destino' : l[1]  
-        }
+        dis = [
+            float(l[2]), l[0], l[1]  
+        ]
         cidadeDistancia.append(dis)
 
 # ordena o dict pela menor distância
-cidadeDistancia = sorted(cidadeDistancia, key=lambda cidadeDistancia: cidadeDistancia['distanc'])
+cidadeDistancia = sorted(cidadeDistancia, key=lambda cidadeDistancia: cidadeDistancia[0])
+
 
 # Verifica se já visitou o vértice, se não add ele na lista de visitados
 cidPercorridas = []
+
+#implementação da adaptação do algoritmo de busca em largura
+def kruskal(grafo):
+    arvore = dict()
+    fila = [grafo[0][0]] # recebe o primeiro elemento
+    visitados = []
+
+    while len(fila) > 0:
+        v = fila.pop(0)
+        visitados.append(v)
+
+    if v not in arvore.keys():
+        arvore[v] = []
+
+        for i in range(len(grafo)):
+            if grafo[i][0] not in visitados:
+                visitados.append(grafo[i][0])
+                fila.append(grafo[i][0])
+                arvore[v].append(grafo[i][0])
+
+    return arvore
+
+cidPercorridas = kruskal(cidadeDistancia)
 
 # mostra cidades percorridas
 for i in cidPercorridas:    
